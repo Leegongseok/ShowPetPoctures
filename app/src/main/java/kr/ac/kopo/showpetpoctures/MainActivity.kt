6e0a1557c02ea49c0a8675b1743b488320fe70fa
Leegongseok <seok20108014@gmail.com>
2024-04-11 14:48:29 +0900

    예제4-4

--- app/src/main/java/kr/ac/kopo/showpetpoctures/MainActivity.kt
+++ app/src/main/java/kr/ac/kopo/showpetpoctures/MainActivity.kt
@@ -3,30 +3,37 @@ package kr.ac.kopo.showpetpoctures
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.view.View.INVISIBLE
+import android.view.View.OnClickListener
 import android.widget.Button
 import android.widget.CheckBox
 import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.RadioButton
 import android.widget.RadioGroup
+import android.widget.Switch
 
 class MainActivity : AppCompatActivity() {
-    lateinit var checkStart:CheckBox
+    lateinit var checkStart:Switch
     lateinit var rg:RadioGroup
     lateinit var linearSub:LinearLayout
-    lateinit var btnDone:Button
     lateinit var imgV:ImageView
-
+    lateinit var btnInit:Button
+    lateinit var btnFinish:Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        checkStart =findViewById(R.id.checkStart)
-        rg=findViewById(R.id.rg)
-        linearSub=findViewById<LinearLayout>(R.id.rg)
-        btnDone=findViewById<Button>(R.id.btnDone)
+        checkStart =findViewById<Switch>(R.id.checkStart)
+        rg=findViewById<RadioGroup>(R.id.rg)
+        linearSub=findViewById<LinearLayout>(R.id. linearSub)
         imgV=findViewById<ImageView>(R.id.imgV)
+        btnFinish=findViewById<Button>(R.id.btnFinish)
+        btnInit=findViewById<Button>(R.id.btnInit)
+
+        btnFinish.setOnClickListener(btnListener)
+        btnInit.setOnClickListener(btnListener)
 
         linearSub.visibility=View.INVISIBLE
 
@@ -39,12 +46,22 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
-        btnDone.setOnClickListener{
+       rg.setOnCheckedChangeListener {buttonView,isChecked ->
             when(rg.checkedRadioButtonId){
                 R.id.radiopuppy ->imgV.setImageResource(R.drawable.puppy)
                 R.id.radiokitty1 ->imgV.setImageResource(R.drawable.kitty1)
                 R.id.radiobunny ->imgV.setImageResource(R.drawable.bunny)
             }
+        }
+    }
+    val btnListener=OnClickListener{
+        when(it.id){
+            R.id.btnFinish ->finish()
+            R.id.btnInit -> {
+            checkStart.isChecked=false
+            linearSub.visibility= INVISIBLE
+        }
+
         }
     }
 }
